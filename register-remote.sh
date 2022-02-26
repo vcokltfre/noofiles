76984a061935ea8cef1f3fa8bbdb4e74f14c8252
vcokltfre <vcokltfre@gmail.com>
2022-02-26 16:53:40 +0000

    feat: add remote registry script

---
+++ register-remote.sh
@@ -0,0 +1,4 @@
+noo reg add api https://raw.githubusercontent.com/vcokltfre/noofiles/master/noofiles/api.noofile.yml
+noo reg add bot https://raw.githubusercontent.com/vcokltfre/noofiles/master/noofiles/bot.noofile.yml
+noo reg add worker https://raw.githubusercontent.com/vcokltfre/noofiles/master/noofiles/worker.noofile.yml
+noo reg add svelte https://raw.githubusercontent.com/vcokltfre/noofiles/master/noofiles/svelte.noofile.yml
